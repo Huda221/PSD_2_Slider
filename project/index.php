aea040ba1ffe64f470417e801251eea761c55ea6
Huda Zahid <142784857+Huda221@users.noreply.github.com>
2023-10-02 15:08:02 +0500

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,479 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" type="text/css" href="./slick/slick.css">
+  <link rel="stylesheet" type="text/css" href="./slick/slick-theme.css">
+    <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@700&family=Inter:wght@300&family=Montserrat:wght@200&family=Nanum+Gothic&family=Nunito+Sans:opsz,wght@6..12,300&family=Oswald&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@700&family=Inter:wght@300&family=Montserrat:wght@200&family=Nanum+Gothic&family=Nunito+Sans:opsz,wght@6..12,300&family=Oswald&family=Roboto+Condensed:wght@700&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/8e351b2eb4.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+          $(".mob-menu").click(function(){
+            $(".menu").toggle();
+          });
+        });
+        </script>
+</head>
+<body>
+    <div class="header">
+        <div class="navbar">
+            <div class="container">
+                <div class="logo">
+                    <img src="./images/logo.png" alt="">
+                </div>
+                <div class="mob-menu">
+                    <i class="fa-solid fa-bars"></i>
+                </div>
+                <div class="nav">
+                    <ul class="menu">
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Trust</li>
+                        <li>
+                            Products ⏷
+                            <ul class="submenu">
+                                <li>Home</li>
+                                <li>About Us</li>
+                                <li>Trust</li>
+                                <li>Products</li>
+                                <li>FAQ</li>
+                                <li>Contact Us</li>
+                            </ul>
+                        </li>
+                        <li>FAQ</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <div class="title">
+                <div class="content">
+                    <p>On-Demand</p>
+                    <h1><strong>Protection Equipments</strong></h1>
+                    <p class="hi">Lorem ipsum dolor sit amet consectetur adipisicing elit, sed do eiusmod.</p>
+                </div>
+            </div>
+            <div class="hat">
+                <img src="./images/Hat.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="product">
+       <div class="container">
+            <div class="offer">
+               <h1>Products We Offer</h1>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+           
+            </div>
+            <div class="icons">
+                <i class="fa-light fa-less-than"></i>
+                <i class="fa-light fa-greater-than"></i>
+            </div>
+       </div>
+    </div>
+
+    <div class="slider">
+       <div class="container">
+        <!-- slidrer start -->
+        
+  <section class="regular slider">
+    <div>
+    <div class="helmet">
+                <div class="para">
+                    <h4>Specialized</h4>
+                    <h1>Mining Helmets</h1>
+                    <button class="btn" >More Helmets </button>
+                </div>
+                <div class="pic">
+                    <img src="./images/helmet.png" alt="">
+                </div>
+            </div>
+    </div>
+    <div>
+    <div class="helmet">
+                <div class="para">
+                    <h4>Specialized</h4>
+                    <h1>Mining Helmets</h1>
+                    <button class="btn" >More Helmets </button>
+                </div>
+                <div class="pic">
+                    <img src="./images/helmet.png" alt="">
+                </div>
+            </div>
+    </div>
+    <div>
+    <div class="helmet">
+                <div class="para">
+                    <h4>Specialized</h4>
+                    <h1>Mining Helmets</h1>
+                    <button class="btn" >More Helmets </button>
+                </div>
+                <div class="pic">
+                    <img src="./images/helmet.png" alt="">
+                </div>
+            </div>
+    </div>
+    <div>
+    <div class="helmet">
+                <div class="para">
+                    <h4>Specialized</h4>
+                    <h1>Mining Helmets</h1>
+                    <button class="btn" >More Helmets </button>
+                </div>
+                <div class="pic">
+                    <img src="./images/helmet.png" alt="">
+                </div>
+            </div>
+    </div>
+    <div>
+    <div class="helmet">
+                <div class="para">
+                    <h4>Specialized</h4>
+                    <h1>Mining Helmets</h1>
+                    <button class="btn" >More Helmets </button>
+                </div>
+                <div class="pic">
+                    <img src="./images/helmet.png" alt="">
+                </div>
+            </div>
+    </div>
+    <div>
+    <div class="helmet">
+                <div class="para">
+                    <h4>Specialized</h4>
+                    <h1>Mining Helmets</h1>
+                    <button class="btn" >More Helmets </button>
+                </div>
+                <div class="pic">
+                    <img src="./images/helmet.png" alt="">
+                </div>
+            </div>
+    </div>
+  </section>
+
+        <!-- slider end -->
+        <!-- 
+            <div class="helmet">
+                <div class="para">
+                    <h4>Specialized</h4>
+                    <h1>Mining Helmets</h1>
+                    <button class="btn" >More Helmets </button>
+                </div>
+                <div class="pic">
+                    <img src="./images/helmet.png" alt="">
+                </div>
+            </div>
+            <div class="shoes">
+                <div class="para">
+                    <h4>Exclusive</h4>
+                    <h1>Safety Boots</h1>
+                    <button class="btnn" >More Vesta </button>
+                </div>
+                <div class="pic">
+                    <img src="./images/shoe.png" alt="">
+                </div>
+            </div>
+    -->
+       </div> 
+    </div>
+<div class="slider">
+   <div class="container">
+        <div class="hell">
+            <h1>Top Products</h1>
+            <hr>
+        </div>
+   </div>
+   <div class="container">
+        <div class="top">
+            <img src="./images/farm.png" alt="">
+            <div class="content">
+                <h6>Safety Boots</h6>
+                <h3>Farming Boots</h3>
+                <h4>$160.00</h4>
+            </div>
+        </div>
+        <div class="top">
+            <div class="topimage" style="background-image: url(images/smallhat.png);">
+                <div class="buttons">
+                    <a href="#" class="addcart">Add to cart</a>
+                    <a href="#"><3</a>
+                </div>
+            </div>
+            
+            <div class="content">
+                <h6>Helmets</h6>
+                <h3>Z01 Mining Helmet</h3>
+                <h4>$160.00</h4>
+            </div>
+        </div>
+        <div class="top">
+            <div class="topimage" style="background-image: url(images/Jacket.png);">
+                <div class="buttons">
+                    <a href="#" class="addcart">Add to cart</a>
+                    <a href="#"><3</a>
+                </div>
+            </div>
+            <div class="content">
+                <h6>Bullet Proof Vests</h6>
+                <h3>A1 Bullet Proof</h3>
+                <h4>$160.00</h4>
+            </div>
+        </div>
+        <div class="top">
+            <div class="topimage" style="background-image: url(images/safety.png);">
+                <div class="buttons">
+                    <a href="#" class="addcart">Add to cart</a>
+                    <a href="#"><3</a>
+                </div>
+            </div>
+            <div class="content">
+                <h6>Goggles</h6>
+                <h3>Safety Goggles</h3>
+                <h4>$160.00</h4>
+            </div>
+        </div>
+<!-- second line -->
+        <div class="top">
+            <div class="topimage" style="background-image: url(images/boot.png);">
+                <div class="buttons">
+                    <a href="#" class="addcart">Add to cart</a>
+                    <a href="#"><3</a>
+                </div>
+            </div>
+            <div class="content">
+                <h6>Safety Boots</h6>
+                <h3>Farming Boots</h3>
+                <h4>$160.00</h4>
+            </div>
+        </div>
+        <div class="top">
+            <div class="topimage" style="background-image: url(images/smallhat.png);">
+                <div class="buttons">
+                    <a href="#" class="addcart">Add to cart</a>
+                    <a href="#"><3</a>
+                </div>
+            </div>
+            <div class="content">
+                <h6>Helmets</h6>
+                <h3>Z01 Mining Helmet</h3>
+                <h4>$160.00</h4>
+            </div>
+        </div>
+        <div class="top">
+            <div class="topimage" style="background-image: url(images/Jacket.png);">
+                <div class="buttons">
+                    <a href="#" class="addcart">Add to cart</a>
+                    <a href="#"><3</a>
+                </div>
+            </div>
+            <div class="content">
+                <h6>Bullet Proof Vests</h6>
+                <h3>A1 Bullet Proof</h3>
+                <h4>$160.00</h4>
+            </div>
+        </div>
+        <div class="top">
+            <div class="topimage" style="background-image: url(images/safety.png);">
+                <div class="buttons">
+                    <a href="#" class="addcart">Add to cart</a>
+                    <a href="#"><3</a>
+                </div>
+            </div>
+            <div class="content">
+                <h6>Goggles</h6>
+                <h3>Safety Goggles</h3>
+                <h4>$160.00</h4>
+            </div>
+        </div>
+    </div>
+</div>   
+
+<div class="orange">
+    <div class="container">
+        <div class="content">
+            <h1>T-Series Taser Gun 30% OFF</h1>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed magna eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div class="time">
+                <ul>
+                    <li><h1>02</h1></li>
+                    <li><h1>:</h1></li>
+                    <li><h1>14</h1></li>
+                    <li><h1>:</h1></li>
+                    <li><h1>01</h1></li>
+                    <li><h1>:</h1></li>
+                    <li><h1>29</h1></li>
+                </ul>
+            </div>
+            <ul>
+                <li>DAYS</li>
+                <li>HOURS</li>
+                <li>MINUTES</li>
+                <li>SECONDS</li>
+            </ul>
+            <button>Shop Now</button>
+        </div>
+        <div class="image">
+            <img src="./images/Taser.png" alt="">
+        </div>
+    </div>
+</div>
+
+<div class="sliders">
+    <div class="container">
+        <div class="hell">
+            <h1>How we work</h1>
+            <hr>
+        </div>
+   </div>
+   <div class="container">
+        <div class="top">
+            <img src="./images/medal.png" alt="">
+            <div class="content">
+                <h3>Quality Items</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+        </div>
+
+        <div class="top">
+            <img src="./images/stock.png" alt="">
+            <div class="content">
+                <h3>Items in Stock</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+        </div>
+
+        <div class="top">
+            <img src="./images/ship.png" alt="">
+            <div class="content">
+                <h3>Fast Shipping</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+        </div>
+
+        <div class="top">
+            <img src="./images/day.png" alt="">
+            <div class="content">
+                <h3>Same Day Handling</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+        </div>
+   </div>
+</div>
+
+<div class="pictures">
+    <div class="container">
+        <div class="hell">
+            <h1>Featured Clients</h1>
+            <hr>
+        </div>
+   </div>
+   <div class="container">
+        <div class="top">
+            <img src="./images/USA.png" alt="">
+        </div>
+        <div class="top">
+            <img src="./images/air.png" alt="">
+        </div>
+        <div class="top topp">
+            <img src="./images/JUSTICE.png" alt="">
+        </div>
+        <div class="top topper">
+            <img src="./images/police.png" alt="">
+        </div>
+   </div>
+
+   <div class="container">
+        <div class="top">
+            <img src="./images/armoury.png" alt="">
+        </div>
+        <div class="top">
+            <img src="./images/rescue.png" alt="">
+        </div>
+        <div class="top topp">
+            <img src="./images/security.png" alt="">
+        </div>
+        <div class="top topper">
+            <img src="./images/law.png" alt="">
+        </div>
+    </div>
+</div>
+
+<div class="slide">
+    <div class="container">
+        <div class="content">
+            <h1>ʻʻ</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <div class="line">
+                <h5>-    John Doe</h5>
+                <h5 class="in">Senior Inspector</h5>
+            </div>
+        </div>
+        <div class="img">
+            <img src="./images/Stock photo.png" alt="">
+        </div>
+    </div>
+</div>
+<div class="foot">
+    <div class="navbar">
+        <div class="container">
+            <div class="logo">
+                <img src="./images/logo.png" alt="">
+            </div>
+            <div class="nav">
+                <ul class="menu">
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Trust</li>
+                    <li>
+                        Products ⏷
+                        <ul class="submenu">
+                            <li>Home</li>
+                            <li>About Us</li>
+                            <li>Trust</li>
+                            <li>Products</li>
+                            <li>FAQ</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </li>
+                    <li>FAQ</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer>
+    <div class="container flex">
+        <div class="p">
+            <p>&copy; 2019 All Rights Reserved</p>
+        </div>
+        <div class="content">
+            <p>Terms & Conditions</p>
+            <p>Privacy Policy</p>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+  <script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>
+  <script type="text/javascript">
+    $(document).on('ready', function() {
+      $(".regular").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2
+      });
+    });
+</script>
+
+</footer>
+</body>
+</html>
